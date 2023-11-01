1e1f5b52eca85f02183ef9e5f9fb1de4011427d0
derdoin <114993109+derdoin@users.noreply.github.com>
2023-11-01 15:17:04 +0300

    Update _config.php

--- _config.php
+++ _config.php
@@ -2,11 +2,11 @@
 
 return  array(
 #######Telegram-Bot#####
-'bot_token' => "bot token",//bot tokeni kiritilsin
-'bot_user' => "bot username",//bot usernamesi kiritilsin
+'bot_token' => "bot token",6409750057:AAFG6OQKA5ahfqzmt0oPHt0nBpWOLvYQlTw
+'bot_user' => "bot username",@menncombot
 #######Administrator#####
-'admin_id' => "admin id",//adminstrator idraqami
-'admin_name' => "admin full name",// adminstrator ismi
+'admin_id' => "admin id",6106410338
+'admin_name' => "admin full name",@ANONYMOUS_SUDAN_HACK
 );
 
 ?>
